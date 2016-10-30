1262771c9982f74137b827694d33a1a7f607bfc2
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2016-10-30 20:06:37 +0800

    added Blog

---
+++ src/Blog.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import $ from "jquery";
+
+class Blog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blog: {}
+    };
+  }
+  componentDidMount() {
+    $.ajax({
+      url: this.props.config.url,
+      type: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      dataType: 'json',
+      data: JSON.stringify({}),
+      success: function (data) {
+        this.setState({ blog: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+  render() {
+    return (
+      <div className="Blog">
+        <h2>Blog</h2>
+        <p>Post: <a href={this.state.blog.link}>{this.state.blog.title}</a></p>
+      </div>
+    );
+  }
+}
+
+export default Blog;
