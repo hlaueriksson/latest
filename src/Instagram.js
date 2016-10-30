5a2d820de0ae26bc0a33882a55541acfee7c005e
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2016-10-30 20:52:44 +0800

    added Instagram

---
+++ src/Instagram.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import $ from "jquery";
+import './Instagram.css';
+
+class Instagram extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            instagram: {}
+        };
+    }
+    componentDidMount() {
+        $.ajax({
+            url: this.props.config.url,
+            type: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            dataType: 'json',
+            data: JSON.stringify({}),
+            success: function (data) {
+                this.setState({ instagram: data });
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    }
+    createMarkup() {
+        return { __html: this.state.instagram.html };
+    }
+    render() {
+        return (
+            <div className="Instagram" ref="instagram" dangerouslySetInnerHTML={this.createMarkup()} />
+        );
+    }
+    componentDidUpdate() {
+        const script = document.createElement('script');
+        script.async = true;
+        script.defer = true;
+        script.src = '//platform.instagram.com/en_US/embeds.js';
+
+        const node = ReactDOM.findDOMNode(this.refs.instagram);
+        node.parentNode.appendChild(script);
+    }
+}
+
+export default Instagram;
