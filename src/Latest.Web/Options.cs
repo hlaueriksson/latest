095cd30264b813f63c133dd2f96d5226a170b5dc
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2022-09-16 22:53:00 +0200

    blazor

---
+++ src/Latest.Web/Options.cs
@@ -0,0 +1,33 @@
+namespace Latest.Web
+{
+    public class BlogOptions
+    {
+        public string Url { get; set; }
+    }
+
+    public class GitHubOptions
+    {
+        public string Url { get; set; }
+    }
+
+    public class GravatarOptions
+    {
+        public string Url { get; set; }
+        public string Alt { get; set; }
+    }
+
+    public class InstagramOptions
+    {
+        public string Url { get; set; }
+    }
+
+    public class SpotifyOptions
+    {
+        public string Url { get; set; }
+    }
+
+    public class TwitterOptions
+    {
+        public string Url { get; set; }
+    }
+}
--- src/Latest.Web/Program.cs
+++ src/Latest.Web/Program.cs
@@ -6,6 +6,20 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Options
+var http = new HttpClient()
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+};
+builder.Services.AddScoped(sp => http);
+using var response = await http.GetAsync("appsettings.json");
+using var stream = await response.Content.ReadAsStreamAsync();
+builder.Configuration.AddJsonStream(stream);
+builder.Services.AddOptions<BlogOptions>().Bind(builder.Configuration.GetSection(nameof(BlogOptions)));
+builder.Services.AddOptions<GitHubOptions>().Bind(builder.Configuration.GetSection(nameof(GitHubOptions)));
+builder.Services.AddOptions<GravatarOptions>().Bind(builder.Configuration.GetSection(nameof(GravatarOptions)));
+builder.Services.AddOptions<InstagramOptions>().Bind(builder.Configuration.GetSection(nameof(InstagramOptions)));
+builder.Services.AddOptions<SpotifyOptions>().Bind(builder.Configuration.GetSection(nameof(SpotifyOptions)));
+builder.Services.AddOptions<TwitterOptions>().Bind(builder.Configuration.GetSection(nameof(TwitterOptions)));
 
 await builder.Build().RunAsync();
