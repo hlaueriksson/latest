75a5cb6fec0db2c26b2b81835dd8c1fc7100359b
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2022-09-17 13:57:29 +0200

    function

---
+++ src/Latest.Functions/Options.cs
@@ -0,0 +1,17 @@
+namespace Latest.Functions
+{
+    public class BlogQueryOptions
+    {
+        public string FeedUrl { get; set; }
+    }
+
+    public class GitHubQueryOptions
+    {
+        public string Username { get; set; }
+    }
+
+    public class InstagramQueryOptions
+    {
+        public string Username { get; set; }
+    }
+}
---
+++ src/Latest.Functions/Query.cs
@@ -0,0 +1,23 @@
+using CommandQuery.AzureFunctions;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Latest.Functions
+{
+    public class Query
+    {
+        private readonly IQueryFunction _queryFunction;
+        private readonly ILogger _logger;
+
+        public Query(IQueryFunction queryFunction, ILoggerFactory loggerFactory)
+        {
+            _queryFunction = queryFunction;
+            _logger = loggerFactory.CreateLogger<Query>();
+        }
+
+        [Function("Query")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "query/{queryName}")] HttpRequestData req, FunctionContext executionContext, string queryName) =>
+            await _queryFunction.HandleAsync(queryName, req, _logger);
+    }
+}
